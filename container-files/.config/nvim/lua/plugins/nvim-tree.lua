1d2a8d3d4f41c452cdcd64b58d99dfe280c99e75
mijinko <kougouseikeidansi@gmail.com>
2024-04-28 05:03:29 +0000

    Register nvim tree keybind.

--- container-files/.config/nvim/lua/plugins/nvim-tree.lua
+++ container-files/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = true
+  config = function()
+    local api = require('nvim-tree.api')
+    vim.keymap.set('n', '<leader>tr', api.tree.toggle, {})
+    require("nvim-tree").setup({})
+  end
 }
